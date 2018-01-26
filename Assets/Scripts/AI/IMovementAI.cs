0915d61585cc3a9a82f1a6a02b1617adc4b2020d
David Smith <david@flave.co.uk>
2018-01-26 21:11:14 +0000

    Adding the interface design for the AI Pathfinding

---
+++ Assets/Scripts/AI/IMovementAI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    public interface IMovementAI
+    {
+        List<Node> CreatePath(Vector3 source, Vector3 target);
+    }
+}
