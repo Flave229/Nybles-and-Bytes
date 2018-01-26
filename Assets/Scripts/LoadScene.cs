df9ff205a432f8e103c8655675e998cd0460a07c
Malcom-Nubbins <malcom.nubbins@gmail.com>
2018-01-26 21:24:37 +0000

    Fixed LoadScene Script

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwapScene : MonoBehaviour {
+public class LoadScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
