4b680a9cec9e0b492e87dfabe8dec2cc9946e5bf
Malcom-Nubbins <malcom.nubbins@gmail.com>
2018-01-26 21:20:43 +0000

    Added skeleton classes for scene switching. Added Main Menu Scene

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenes : MonoBehaviour {
+
+    public enum Scene
+    {
+        MAIN_MENU = 0,
+        GAME = 1,
+        GAME_OVER = 2
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
