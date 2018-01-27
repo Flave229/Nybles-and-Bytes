f650bc36f378b08dc6730fb20d62d33ada91ce10
Malcom-Nubbins <malcom.nubbins@gmail.com>
2018-01-27 23:31:09 +0000

    Field of view is now visible on the security camera. Still a bit weird when the camera is rotated.

--- Assets/Scripts/Electronics/Security_Camera.cs
+++ Assets/Scripts/Electronics/Security_Camera.cs
@@ -1,10 +1,12 @@
 ﻿using Assets.Scripts;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Security_Camera : MonoBehaviour, ICircuitComponent
 {
     public int detectDistance;
+    public float FieldOfView;
 
     private Vector3 securityCameraPos;
     private Collider[] cols;
@@ -19,6 +21,9 @@ public class Security_Camera : MonoBehaviour, ICircuitComponent
     private PlayerCameraController _mCamera;
     [SerializeField]
     private Transform _lineOfSight;
+
+    public Transform FovLeftEdge;
+    public Transform FovRightEdge;
 
     public void Execute()
     {
@@ -60,6 +65,8 @@ public class Security_Camera : MonoBehaviour, ICircuitComponent
 
         securityCameraPos = transform.position;
         cols = Physics.OverlapSphere(securityCameraPos, detectDistance);
+
+        //transform.Rotate(new Vector3(0.0f, 0.0f, 0.2f));
         foreach (Collider col in cols)
         {
             if (col != null)
@@ -68,15 +75,55 @@ public class Security_Camera : MonoBehaviour, ICircuitComponent
                 if (col.gameObject.GetComponent<UniquePlayerCTRL>() != null)
                 {
                     Vector2 targetDir = col.gameObject.transform.position - transform.position;
-                   // Debug.DrawLine(transform.position, col.gameObject.transform.position, Color.green);
+                    Debug.DrawLine(transform.position, _lineOfSight.position, Color.blue);
 
                     Vector2 lineOfSight = _lineOfSight.position - transform.position;
-                    Debug.DrawLine(transform.position, _lineOfSight.position, Color.red);
-
-                    float angleToPlayer = Vector2.Angle(targetDir, lineOfSight);
-                    
-                    if (angleToPlayer < 65)
+                    //lineOfSight.Normalize();
+                    float angleToLineOfSight;
+                    if(transform.rotation.z < 0 && transform.rotation.z > -180)
                     {
+                        angleToLineOfSight = Vector2.Angle(Vector2.right, lineOfSight);
+                    }
+                    else
+                    {
+                        angleToLineOfSight = Vector2.Angle(Vector2.up, lineOfSight);
+                    }
+                    
+                    float cos = (float)Math.Cos((Math.PI / 180.0f) * (angleToLineOfSight + FieldOfView));
+                    float cos2 = (float)Math.Cos((Math.PI / 180.0f) * (angleToLineOfSight - FieldOfView));
+                    float sin = (float)Math.Sin((Math.PI / 180.0f) * (angleToLineOfSight + FieldOfView));
+                    float sin2 = (float)Math.Sin((Math.PI / 180.0f) * (angleToLineOfSight - FieldOfView));
+                    float hypotenuse = Vector2.Distance(_lineOfSight.position, transform.position);
+                    Vector2 fovEdge1;
+                    Vector2 fovEdge2;
+                    if (transform.rotation.z < 0 && transform.rotation.z > -180)
+                    {
+                        fovEdge1 = new Vector2(hypotenuse * cos, hypotenuse * -sin);
+                        fovEdge2 = new Vector2(hypotenuse * cos2, hypotenuse * -sin2);
+                    }
+                    else
+                    {
+                        fovEdge1 = new Vector2(hypotenuse * -cos, hypotenuse * -sin);
+                        fovEdge2 = new Vector2(hypotenuse * -cos2, hypotenuse * -sin2);
+                    }
+
+                    fovEdge1 = Vector3.Normalize(fovEdge1);
+                    fovEdge2 = Vector3.Normalize(fovEdge2);
+
+                    fovEdge1 *= detectDistance;
+                    fovEdge2 *= detectDistance;
+
+                    Vector2 fovLine1Pos = (Vector2)transform.position + fovEdge1;
+                    Vector2 fovLine2Pos = (Vector2)transform.position + fovEdge2;
+                    
+                    Debug.DrawLine(transform.position, fovLine1Pos, Color.red);
+                    Debug.DrawLine(transform.position, fovLine2Pos, Color.red);
+
+                    float angleToPlayer = Vector2.Angle(targetDir, lineOfSight * detectDistance);
+
+                    if (angleToPlayer < FieldOfView)
+                    {
+                        
                         Debug.DrawLine(transform.position, col.gameObject.transform.position, Color.green);
                         col.gameObject.GetComponent<UniquePlayerCTRL>().DetectedByCamera();
                     } 
@@ -85,16 +132,22 @@ public class Security_Camera : MonoBehaviour, ICircuitComponent
                 }
                 else
                 {
+                    Vector2 lineOfSight = _lineOfSight.position - transform.position;
+                    Vector2 targetDir = col.gameObject.transform.position - transform.position;
+                    float angleToPlayer = Vector2.Angle(targetDir, lineOfSight * detectDistance);
                     // Clones
                     if (col.gameObject.GetComponent<PlayerCTRL>() != null)
                     {
-                        col.gameObject.GetComponent<PlayerCTRL>().DetectableBehaviour.Detected();
+                        if (angleToPlayer < FieldOfView)
+                        {
 
-                        _mCloneIndex = GameManager.Instance().GetListOfEntities().Count - 1;
-                        _mCloneIndex -= 1;
-                        _mCamera.SetTargetPlayerObject(GameManager.Instance().GetListOfEntities()[_mCloneIndex]);
-                        GameManager.Instance().GetListOfEntities()[_mCloneIndex].SetUserControlEnabled(true);
+                            col.gameObject.GetComponent<PlayerCTRL>().DetectableBehaviour.Detected();
 
+                            _mCloneIndex = GameManager.Instance().GetListOfEntities().Count - 1;
+                            _mCloneIndex -= 1;
+                            _mCamera.SetTargetPlayerObject(GameManager.Instance().GetListOfEntities()[_mCloneIndex]);
+                            GameManager.Instance().GetListOfEntities()[_mCloneIndex].SetUserControlEnabled(true);
+                        }
                     }
                 }                                
             }
