7fc1d4d0b0c2bb1a242a162e54ede46ccf7381e7
Malcom-Nubbins <malcom.nubbins@gmail.com>
2018-01-28 05:44:42 +0000

    Security Camera now works properly when rotated.

--- Assets/Scripts/Electronics/Security_Camera.cs
+++ Assets/Scripts/Electronics/Security_Camera.cs
@@ -22,9 +22,6 @@ public class Security_Camera : MonoBehaviour, ICircuitComponent
     [SerializeField]
     private Transform _lineOfSight;
 
-    public Transform FovLeftEdge;
-    public Transform FovRightEdge;
-
     public void Execute()
     {
         Enabled = !Enabled;
@@ -63,6 +60,9 @@ public class Security_Camera : MonoBehaviour, ICircuitComponent
         if (Enabled == false)
             return;
 
+        if (transform.rotation.z > 360)
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+
         securityCameraPos = transform.position;
         cols = Physics.OverlapSphere(securityCameraPos, detectDistance);
 
@@ -78,28 +78,30 @@ public class Security_Camera : MonoBehaviour, ICircuitComponent
                     Debug.DrawLine(transform.position, _lineOfSight.position, Color.blue);
 
                     Vector2 lineOfSight = _lineOfSight.position - transform.position;
-                    //lineOfSight.Normalize();
+                    lineOfSight.Normalize();
                     float angleToLineOfSight;
-                    if(transform.rotation.z < 0 && transform.rotation.z > -180)
+                    
+                    if(transform.eulerAngles.z >= 0 && transform.eulerAngles.z <= 180)
                     {
                         angleToLineOfSight = Vector2.Angle(Vector2.right, lineOfSight);
                     }
                     else
                     {
-                        angleToLineOfSight = Vector2.Angle(Vector2.up, lineOfSight);
+                        angleToLineOfSight = Vector2.Angle(-Vector2.right, lineOfSight);
                     }
                     
-                    float cos = (float)Math.Cos((Math.PI / 180.0f) * (angleToLineOfSight + FieldOfView));
-                    float cos2 = (float)Math.Cos((Math.PI / 180.0f) * (angleToLineOfSight - FieldOfView));
-                    float sin = (float)Math.Sin((Math.PI / 180.0f) * (angleToLineOfSight + FieldOfView));
-                    float sin2 = (float)Math.Sin((Math.PI / 180.0f) * (angleToLineOfSight - FieldOfView));
+                    float cos = (float)Math.Cos((Math.PI / 180.0f) * (angleToLineOfSight - FieldOfView));
+                    float cos2 = (float)Math.Cos((Math.PI / 180.0f) * (angleToLineOfSight + FieldOfView));
+                    float sin = (float)Math.Sin((Math.PI / 180.0f) * (angleToLineOfSight - FieldOfView));
+                    float sin2 = (float)Math.Sin((Math.PI / 180.0f) * (angleToLineOfSight + FieldOfView));
                     float hypotenuse = Vector2.Distance(_lineOfSight.position, transform.position);
                     Vector2 fovEdge1;
                     Vector2 fovEdge2;
-                    if (transform.rotation.z < 0 && transform.rotation.z > -180)
+
+                    if (transform.eulerAngles.z >= 0 && transform.eulerAngles.z <= 180)
                     {
-                        fovEdge1 = new Vector2(hypotenuse * cos, hypotenuse * -sin);
-                        fovEdge2 = new Vector2(hypotenuse * cos2, hypotenuse * -sin2);
+                        fovEdge1 = new Vector2(hypotenuse * cos, hypotenuse * sin);
+                        fovEdge2 = new Vector2(hypotenuse * cos2, hypotenuse * sin2);
                     }
                     else
                     {
