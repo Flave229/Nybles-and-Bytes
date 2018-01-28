2c7075faf3d5cc25d267c4a96507c5fafcbc69bf
Malcom-Nubbins <malcom.nubbins@gmail.com>
2018-01-28 08:50:31 +0000

    DataPacket should no longer fall through the floor. Now has gravity applied to it

--- Assets/Scripts/GameLogic/PickupUSB.cs
+++ Assets/Scripts/GameLogic/PickupUSB.cs
@@ -22,6 +22,7 @@ public class PickupUSB : MonoBehaviour {
     {
         if (PickedUp)
         {
+            GetComponent<Rigidbody>().useGravity = false;
             //if (transform.localScale.x > 0.15f)
             //	transform.localScale-=new Vector3(0.01f, 0.01f, 0);
             //transform.localPosition = new Vector3(0, 6.6666f*(1-2.2222f*(transform.localScale.x-0.15f)) );
@@ -33,10 +34,8 @@ public class PickupUSB : MonoBehaviour {
         }
         else
         {
-            if (transform.localPosition.y > 1.44f)
-                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 0.33f);
-            else
-                transform.localPosition = new Vector3(transform.localPosition.x, 1.44f);
+            GetComponent<Rigidbody>().useGravity = true;
+            
         }
     }
 
